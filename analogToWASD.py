0533e9ed44abadd225d6ba1d9b31877d26e47616
KJK <kody.j.king@gmail.com>
2022-01-23 20:22:43 -0800

    per axis maximum values

--- analogToWASD.py
+++ analogToWASD.py
@@ -5,19 +5,19 @@ import threading
 
 pyautogui.PAUSE = 0
 
-axisMax = 32768
 analogEnabled = False
-epsilon = 0.1
-intervalLength = 0.1
+epsilon = 0.01
+intervalLength = 0.05
 downTime = 0.01
 
 axisList = []
 class Axis:
-    def __init__(self, low, high, eventCode):
+    def __init__(self, low, high, eventCode, axisMax):
         self.low = low
         self.high = high
         self.state = 0
         self.eventCode = eventCode
+        self.axisMax = axisMax
 
         axisList.append(self)
 
@@ -33,20 +33,22 @@ class Axis:
 def axisThread(axis):
     while True:
         if analogEnabled:
-            percent = abs(axis.state / axisMax)
+            percent = abs(axis.state / axis.axisMax)
+            print(percent)
             interval = (1 - percent) * intervalLength + downTime
             if percent > epsilon:
                 press(axis.key(), interval = interval, _pause = False)
 
-xAxis = Axis("num4", "num6", "ABS_RX")
-yAxis = Axis("num5", "num8", "ABS_RY")
-# zAxis = Axis("0", "e", "ABS_Y")666666
+xAxis = Axis("num4", "num6", "ABS_RX", 32768)
+yAxis = Axis("num8", "num5", "ABS_RY", 32768)
+zAxis = Axis("s", "w", "ABS_Y", 32768)
 
 buttonMap = {
-    "BTN_TL": "e",
-    "BTN_TR": "q",
+    "BTN_TR": "e",
+    "BTN_TL": "w",
     "BTN_SELECT": "space",
-    "BTN_NORTH": "1"
+    "BTN_NORTH": "1",
+    "BTN_SOUTH": "2"
 }
 
 while True:
@@ -55,7 +57,9 @@ while True:
         for axis in axisList:
             axis.onEvent(event)
 
-        # print(event.code)
+        # code = event.code
+        # if code.startswith("ABS"):
+        #     print(event.code + ": " + str(event.state))
 
         if event.code == "BTN_START" and event.state == 1:
             analogEnabled = not analogEnabled
